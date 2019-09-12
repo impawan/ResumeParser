3abe3373735f7a86223134f636041d2ed15d7fce
Pawan Prasad <pawanprasad@outlook.com>
2019-09-12 16:18:10 +0530

    Add files via upload

--- poilt.py
+++ poilt.py
@@ -16,11 +16,36 @@ from pdfminer.pdfpage import PDFPage,PDFTextExtractionNotAllowed
 import io
 import nltk
 import spacy
+import re
 from spacy.matcher import Matcher
+import pandas as pd
+import numpy as np
 
 nlp = spacy.load('en')
 
 matcher = Matcher(nlp.vocab)
+
+skills = pd.read_csv("one_word_skill.csv")
+
+sklills_list = skills.skill.tolist()
+def extract_email(text):
+    email = re.findall("([^@|\s]+@[^@]+\.[^@|\s]+)", text)
+    if email:
+        try:
+            return email[0].split()[0].strip(';')
+        except IndexError:
+            return None
+
+
+def extract_phone_number(text):
+    phone = re.findall(re.compile(r'(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'), text)
+    
+    if phone:
+        number = ''.join(phone[0])
+        if len(number) > 10:
+            return '+' + number
+        else:
+            return number
 
 def extract_name(resume_text):
     nlp_text = nlp(resume_text)
@@ -61,4 +86,8 @@ def text_cleaning(text):
         
         
 
-
+def extrract_skill(text):
+    for words in text.split():
+        if words in sklills_list:
+            print(words)
+    
